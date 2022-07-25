bbd2dabb6c945f389d87d0f82cd97824d1160257
OoThan <mgoothan.psnl@gmail.com>
2022-07-24 23:59:01 -0700

    class parameter properties

---
+++ 04-classes/004-parameter-properties/Customers.js
@@ -0,0 +1,30 @@
+"use strict";
+var Customer4 = /** @class */ (function () {
+    function Customer4(_firstName, _lastName) {
+        this._firstName = _firstName;
+        this._lastName = _lastName;
+    }
+    Object.defineProperty(Customer4.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer4.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer4;
+}());
+var customer4 = new Customer4("Martin", "Dixon");
+console.log("".concat(customer4.firstName, " ").concat(customer4.lastName));
---
+++ 04-classes/004-parameter-properties/Customers.ts
@@ -0,0 +1,24 @@
+class Customer4 {
+        
+    constructor(private _firstName: string,
+                 private _lastName: string) {
+    }
+    
+    public get firstName(): string {
+        return this._firstName;
+    }
+    public set firstName(value: string) {
+        this._firstName = value;
+    }
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+    public set lastName(value: string) {
+        this._lastName = value;
+    }
+}
+
+let customer4 = new Customer4("Martin", "Dixon");
+
+console.log(`${customer4.firstName} ${customer4.lastName}`);
