9519f182b26e63c43826032ada7ba26679252a30
OoThan <mgoothan.psnl@gmail.com>
2022-07-25 00:14:04 -0700

    modules

---
+++ 04-classes/005-modules/Drivers.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Customers_1 = require("./Customers");
+var customer5 = new Customers_1.Customer5("Kyi", "Kyi");
+console.log("".concat(customer5.firstName, " ").concat(customer5.lastName));
---
+++ 04-classes/005-modules/Drivers.ts
@@ -0,0 +1,5 @@
+import { Customer4 } from "./Customers";
+
+let customer = new Customer4("Kyi", "Kyi");
+
+console.log(`${customer.firstName} ${customer.lastName}`)
